37961361155b841a760624332c577af0de915e15
Beginners-Luck123 <dynieme.fms@gmail.com>
2023-09-16 17:28:09 -0400

    change

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text> Hi Dee </Text>
-      <StatusBar style="auto" />
+git       <StatusBar style="auto" />
     </View>
   );
 }
