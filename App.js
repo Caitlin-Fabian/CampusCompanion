d20a2739e9b4ef9bc44c0a9a453346f4d747ce9d
Caitlin-Fabian <caitlinfstar@knights.ucf.edu>
2023-09-16 03:00:44 -0400

    Navigation

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from './node_modules/@react-navigation/native';
+import {createNativeStackNavigator} from './node_modules/@react-navigation/native-stack';
+import { HomeScreen } from './pages/homeScreen';
+import { LoginScreen } from './pages/loginScreen';
+import { Button } from 'react-native';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+export default MyStack = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
